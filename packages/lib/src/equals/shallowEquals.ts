2a68809f2aaf525acb244efdf7fdad55321ff180
sangsoo <wsckm1@gmail.com>
2025-07-21 13:33:54 +0900

    feat: shallowEquals 구현

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,28 @@
+import shallowEqualsArrays from "./shallowEqualsArrays.ts";
+import shallowEqualsObjects from "./shallowEqualsObjects.ts";
+import { typeUtils } from "../utils/typeUtil.ts";
+
 export const shallowEquals = (a: unknown, b: unknown) => {
-  return a === b;
+  // 원시 타입인 경우
+  if (typeUtils.isPrimitive(a) || typeUtils.isPrimitive(b)) {
+    return a === b;
+  }
+
+  const aIsArray = Array.isArray(a);
+  const bIsArray = Array.isArray(b);
+
+  // 비교하려는 값의 타입이 다르면 false
+  if (aIsArray !== bIsArray) {
+    return false;
+  }
+
+  // 배열인 경우
+  if (aIsArray && bIsArray) {
+    return shallowEqualsArrays(a, b);
+  }
+
+  // 객체인 경우
+  if (typeUtils.isObject(a) && typeUtils.isObject(b)) {
+    return shallowEqualsObjects(a, b);
+  }
 };
